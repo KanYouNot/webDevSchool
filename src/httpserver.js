d8b0e74b108731030e21f8f6aeaaff7b058bc5ae
KanYouNot <167652007+KanYouNot@users.noreply.github.com>
2025-06-12 08:22:24 +0200

    feat

--- src/httpserver.js
+++ src/httpserver.js
@@ -8,18 +8,24 @@ http.createServer((req, res) => {
 
 	switch (req.url) {
 		case '/':
-			res.writeHead(200, {"Content-Type": "text/html"});
-			res.write(fs.writeFileSync(path.join("./index.html")));
+			const htmlPath = path.join("./index.html")
+			const htmlContent = fs.readFileSync(htmlPath)
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(htmlContent)
 			break;
 
 		case '/style.css':
+			const cssPath = path.join("./styles.css")
+			const cssContent = fs.readFileSync(cssPath)
 			res.writeHead(200, {'Content-Type': 'text/css'});
-			res.write(fs.readFileSync(path.join("./styles.css")));
+			res.write(cssContent)
 			break;
 
 		case '/script.js':
+			const jsPath = path.join("./script.js")
+			const jsContent = fs.readFileSync(jsPath)
 			res.writeHead(200, {'Content-Type': 'text/javascript'});
-			res.write(fs.readFileSync(path.join("./script.js")));
+			res.write(jsContent)
 			break;
 
 		default:
@@ -28,7 +34,7 @@ http.createServer((req, res) => {
 	res.end();
 }).listen(port, (err) => {
     if (err) {
-        console.log(`Error: ${err}`);
+        console.log(`Error: ${err}`)
     } else {
         console.log(`Server listening at port 127.0.0.1:${port}`);
     }});
